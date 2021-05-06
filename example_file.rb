e067d5d8a9d692949ba633f6a2a8ba790dffc738
Megan Lo <hkmeganlo@gmail.com>
2021-05-06 07:01:15 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,38 @@
+#puts "Hello World"
+
+
+# def greeting_a_person(name)
+#   puts "Hello #{name}"
+# end
+
+# # greeting_a_person("Maria")
+# # => "Hello Maria"
+
+# name = "Maria"
+# greeting_a_person(name)
+# # => "Hello Maria"
+
+# def stylish_chef
+#   best_hairstyle = "Guy Fieri"
+#   return "Martha Stewart"
+#   "Guy Fieri"
+# end
+
+# stylish_chef
+
+# def a_method(a,b)
+#   puts "I got #{a}"
+#   puts "I got #{b}"
+#   sum = a + b
+#   puts "I got #{sum}"
+#   #sum
+# end
+
+# a_method(2,3)
+
+#Nested Arrays
+spice_rack = [
+  ["Mace", "Ginger", "Marojam"],          
+  ["Paprika", "Fajita Mix", "Coriander"], 
+  ["Parsley", "Sage", "Rosemary"]         
+]
